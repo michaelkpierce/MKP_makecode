760de80e5308bbc1b500b765c8c42d67020bf3a1
michaelkpierce <michaelkpierce@gmail.com>
2023-04-12 10:37:46 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-forever(function () {
+function doSomething (num: number, num2: number, num3: number) {
 	
+}
+forever(function () {
+    pause(100)
+    pause(100)
+    pause(100)
+    pause(100)
 })
